e5ec57e1997d11ccac6ac88addcd234034f819e9
AvinFajarF <avinfajar6@gmail.com>
2023-06-09 11:04:16 +0700

    add greet.go

--- greet/client/main.go
+++ greet/client/main.go
@@ -1,14 +1,15 @@
-package client
+package main
 
 import (
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"log"
 )
 
 func main() {
 	var address string = "localhost:50051"
 
-	conn, err := grpc.Dial(address)
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
 	if err != nil {
 		log.Fatalf("Failed to connection %v \n", err.Error())
